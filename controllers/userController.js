3486872cc502446d41c9968570e71bdecdefc023
Duxpiex <hyukjin6143@gmail.com>
2023-08-10 13:50:52 +0900

    비밀번호 암호화를 사용하여 기본적인 로그인, 로그아웃, 회원가입, 회원탈퇴 구현 완료

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,27 +2,87 @@ const db = require('../db');
 
 const userController = {};
 
+const bcrypt = require('bcrypt');
+
+
+// 로그인
 userController.login = async (req, res) => {
     const { email, password } = req.body;
 
+    console.log('Received email and password:', email, password);  // 1. Request 검증 로그
+
     try {
-        // 쿼리를 실행하고 결과를 받습니다.
-        const result = await db.query('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password]);
+        // 사용자 정보 가져오기
+        const userData = await db.query('SELECT * FROM users WHERE email = $1', [email]);
 
-        // 쿼리의 반환값을 로그로 출력합니다.
-        console.log("Query result:", result);
+        console.log('User fetched from DB:', userData.rows);  // 2. DB 조회 검증 로그
+        console.log('Full DB query response:', userData);
 
 
-
-        if (result.length > 0) {
-            res.status(200).json({ message: 'Login successful', user: result[0] });
-        } else {
-            res.status(401).json({ message: 'Invalid email or password' });
+        if (!userData || userData.length === 0) {
+            return res.status(400).json({ message: 'Invalid email or password' });
         }
+
+        const userPasswordHash = userData[0].password;
+        console.log('Comparing passwords:', password, userPasswordHash);  // 3. 비밀번호 비교 검증 로그
+
+        // 비밀번호 확인
+        if (!userPasswordHash) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const validPassword = await bcrypt.compare(password, userPasswordHash);
+        console.log('Bcrypt comparison result:', validPassword);  // 4. bcrypt 비교 결과 로그
+
+        if (!validPassword) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful' ,email: userData[0].email});
     } catch (error) {
-        console.error('Error during login:', error.message);
-        console.error(error.stack);
-        res.status(500).json({ message: 'Server error', error: error.message });
+        console.error('Error:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
+
+
+
+
+// 회원가입
+userController.register = async (req, res) => {
+    const { email, password } = req.body;
+
+    // 비밀번호 중복 확인
+    const existingUser = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (existingUser.rowCount > 0) {
+        return res.status(400).json({ message: 'Email already exists' });
+    }
+
+    // 비밀번호 암호화
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await db.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword]);
+    res.status(201).json({ message: 'User created successfully' });
+
+};
+
+// 회원탈퇴
+userController.deleteAccount = async (req, res) => {
+    const { email } = req.body;
+
+    console.log("Received email for deletion:", email);
+
+    const result = await db.result('DELETE FROM users WHERE email = $1 RETURNING *', [email]);
+    console.log(result.rowCount);
+
+
+    if (result.rowCount > 0) {
+        res.status(200).json({ message: 'User deleted successfully'  });
+    } else {
+        res.status(404).json({ message: 'User not found' });
     }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,12 @@ const userController = require('../controllers/userController');
 // 사용자 로그인
 router.post('/login', userController.login);
 
+// 사용자 회원가입
+router.post('/register', userController.register);
+
+// 사용자 회원탈퇴
+router.delete('/delete', userController.deleteAccount);
+
 
 
 module.exports = router;
